ffe615a17e4ca5c09ebbe83ee1e221d606743b8f
Fiijoey <kofimensah3730@gmail.com>
2025-05-24 10:19:47 +0000

    refactor: move invoice actions to a separate file and update import paths

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useActionState } from "react";
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import {
@@ -10,8 +9,9 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice, State } from "@/app/lib/action";
+import { createInvoice, State } from "@/app/lib/actions";
 import { init } from "next/dist/compiled/webpack/webpack";
+import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, error: {} };
